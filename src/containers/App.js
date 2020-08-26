54008806fcfa4c097f1a46143d25f5e4e9074715
caofontaine <benjamin.cao88@gmail.com>
2020-08-26 19:31:51 -0400

    clean up code for gamertag and achievement calls

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,31 +26,47 @@ class App extends Component {
     })
     .then(response => response.json())
     .then(gTagId => {
-      console.log(gTagId);
-      this.setState({gamerTagId: gTagId});
+      // If response returns a 404, meaning gamertag not found to obtain gamertag id,
+      // throw an error.
+      if (gTagId.success === false) {
+        throw new Error ('Gamertag not found.');
+      }
+      else {
+        console.log(gTagId);
+        this.setState({gamerTagId: gTagId});
+      }
+
     })
     .catch(err => {
-      console.log("No gamertag found.");
+      console.log("Error getting gamertag id.");
     });
   }
 
   getAchievements = async () => {
     await this.getGamerTagId();
-    fetch(`/v2/${this.state.gamerTagId}/xbox360games`, {
-      method: 'get',
-      headers: {
-        'X-AUTH': '3a5eb14d1a580dc2d1a0e9b10b31fa5cc5958616',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(x360Ach => {
-      console.log(x360Ach);
-      //this.setState({gamerTagId: gTagId});
-    })
-    .catch(err => {
-      console.log("No Xbox 360 achievements found.");
-    });
+    if (this.state.gamerTagId) {
+      console.log('Gamertag found. Obtaining achievements...');
+
+      fetch(`/v2/${this.state.gamerTagId}/xbox360games`, {
+        method: 'get',
+        headers: {
+          'X-AUTH': '3a5eb14d1a580dc2d1a0e9b10b31fa5cc5958616',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(x360Ach => {
+        console.log(x360Ach);
+        this.setState({achievements: x360Ach.titles});
+        console.log(this.state.achievements);
+      })
+      .catch(err => {
+        console.log("Error getting Xbox 360 achievements.");
+      });
+    }
+    else {
+      console.log('Gamertag not found. No gamertag id to use.')
+    }
   }
 
   render() {
