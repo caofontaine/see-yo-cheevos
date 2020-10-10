2971cc74ba7f0335fb34936d6ea2f50ed854d5b1
caofontaine <benjamin.cao88@gmail.com>
2020-10-10 14:18:30 -0400

    Updates

---
+++ precache-manifest.6e9a0f8083ca28ebdbf2cf1795c48df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b27c27e05364de2b649ec3af789b7b",
+    "url": "/see-yo-cheevos/index.html"
+  },
+  {
+    "revision": "b19629a5772e2105e2c2",
+    "url": "/see-yo-cheevos/static/css/main.5b43234f.chunk.css"
+  },
+  {
+    "revision": "95ccd060b6121ca9b31e",
+    "url": "/see-yo-cheevos/static/js/2.de929259.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/see-yo-cheevos/static/js/2.de929259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19629a5772e2105e2c2",
+    "url": "/see-yo-cheevos/static/js/main.1db0d848.chunk.js"
+  },
+  {
+    "revision": "4a9cdff724937d37c2df",
+    "url": "/see-yo-cheevos/static/js/runtime-main.3349b453.js"
+  }
+]);
